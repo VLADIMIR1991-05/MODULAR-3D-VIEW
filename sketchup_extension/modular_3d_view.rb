e31894847e94a733660de1200df90962ef5b3f22
VLADIMIR1991-05 <lenin19910527@gmail.com>
2026-08-17 21:59:42 -0500

    Agregar extensión MODULAR-3D VIEW para SketchUp

---
+++ sketchup_extension/modular_3d_view.rb
@@ -0,0 +1,15 @@
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module Modular3D
+  module View
+    EXTENSION = SketchupExtension.new(
+      'MODULAR-3D VIEW',
+      File.join('modular_3d_view', 'main')
+    )
+    EXTENSION.description = 'Prepara modelos SketchUp para MODULAR-3D VIEW sin modificar el archivo SKP original.'
+    EXTENSION.version = '0.1.0'
+    EXTENSION.creator = 'MODULAR-3D'
+    Sketchup.register_extension(EXTENSION, true)
+  end
+end
